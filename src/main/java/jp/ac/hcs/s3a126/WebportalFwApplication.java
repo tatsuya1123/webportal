82a845ab4ee9524da86bdf067ddf998434412ee6
tatuya1123 <itsfrash2480@gmail.com>
2021-06-28 09:45:47 +0900

    最初のコミットです

---
+++ src/main/java/jp/ac/hcs/s3a126/WebportalFwApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a126;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebportalFwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebportalFwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/s3a126/WebportalFwApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a126;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebportalFwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
