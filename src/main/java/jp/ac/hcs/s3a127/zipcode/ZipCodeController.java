d9c9787f2f34815718ef68c2a466e89aff6b64cc
tatuya1123 <itsfrash2480@gmail.com>
2021-06-28 14:10:50 +0900

    WebPortal-住所検索機能①

---
+++ src/main/java/jp/ac/hcs/s3a127/zipcode/ZipCodeController.java
@@ -0,0 +1,23 @@
+package jp.ac.hcs.s3a127.zipcode;
+
+import java.security.Principal;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class ZipCodeController {
+	/**
+	 * 郵便番号から住所を検索し、結果画面を表示する
+	 * @param zipcode 検索する郵便番号(ハイフン無し)
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return 結果画面 - 郵便番号
+	 */
+	@PostMapping("/zip")
+	public String getZipCode(@RequestParam("zipcode") String zipcode,
+			Principal principal, Model model) {
+				
+				return "zipcode/zipcode";
+			}
+}
